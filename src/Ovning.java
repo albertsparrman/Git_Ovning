53a41f7aa38f3dc25e7829db6185c855c2a7ec09
albertsparrman <77719327+albertsparrman@users.noreply.github.com>
2021-11-15 11:19:16 +0100

    Jobbade på uppgift 1

--- src/Ovning.java
+++ src/Ovning.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+
 public class Ovning {
+    public static void  main(String[] args){
+        String tal = "";
+        String a = JOptionPane.showInputDialog("Hur manga tal");
+        int x = Integer.parseInt(a);
+        int y = 1;
+        String b = JOptionPane.showInputDialog("Tal " + y);
+        int z = Integer.parseInt(b);
+        int p = 0;
+
+        while (y<=x){
+
+            y++;
+        }
+    }
 }
